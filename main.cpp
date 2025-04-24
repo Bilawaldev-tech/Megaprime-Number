0c842ad652555c06d23dbd043375c9878aadd91f
Bilawaldev-tech <Lateefbilawal7@gmail.com>
2025-04-24 16:36:50 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
 #include <vector>
 #include <limits>
@@ -199,7 +207,7 @@ int main()
    }
    else
    {
-       std::cout << " Invalid number entered";
+       std::cout << "Invalid number entered";
    }
     return 0;
 }
